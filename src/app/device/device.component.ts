5699d8d2fbcda39b5f1034fd331ca07b0ed7c26f
Sushrut Khirwadkar <sushrut.khirwadkar@soch-inc.com>
2018-10-05 09:39:18 -0400

    Device console

--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {APP_CONFIG} from '../app.config';
-import {ApiserviceService} from '../apiservice.service';
-import {Http, HttpModule, Headers, RequestOptions} from '@angular/http';
-import { Device,Server } from '../data_modelDeviceInventory';
+import { APP_CONFIG } from '../app.config';
+import { ApiserviceService } from '../apiservice.service';
+import { Http, HttpModule, Headers, RequestOptions } from '@angular/http';
+import { Device, Server } from '../data_modelDeviceInventory';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UtilService } from '../util.service';
-import { FilterPipe} from '../convertDate.pipe';
+import { FilterPipe } from '../convertDate.pipe';
 import { DatePipe } from '@angular/common';
 import { IMyDate, IMyDpOptions } from 'mydatepicker';
 import * as moment from 'moment';
@@ -21,74 +21,75 @@ export class DeviceComponent implements OnInit {
   device: Device;
   getDevice: Device;
   displayDevices: any;
-    public desc = false;
-      public p: number = 1;
+  public desc = false;
+  public p: number = 1;
   renewalDate: any;
-  serverContact:Server;
-  serverContact1:Server;
-  public startDate:any;
-    public endDate:any;
-    isLol:boolean = false;
-    renDate:any;
-   public myDatePickerOptions: IMyDpOptions = {
+  serverContact: Server;
+  serverContact1: Server;
+  public startDate: any;
+  public endDate: any;
+  isLol: boolean = false;
+  renDate: any;
+  public myDatePickerOptions: IMyDpOptions = {
     dateFormat: 'yyyy-mm-dd'
   };
-  constructor(private _apiservice: ApiserviceService, private  http: Http, private modalService: NgbModal, private utilservice: UtilService,private datepipe: DatePipe) { 
+  constructor(private _apiservice: ApiserviceService, private http: Http, private modalService: NgbModal, private utilservice: UtilService, private datepipe: DatePipe) {
     this.device = new Device();
     this.getDevice = new Device();
-      this.device.serverContactDTOs = [];
-       	this.serverContact = new Server();
-       	this.serverContact1 = new Server();
+    this.device.serverContactDTOs = [];
+    this.serverContact = new Server();
+    this.serverContact1 = new Server();
   }
 
   ngOnInit() {
     //for(let i = 1;i <= UtilService.noIds;i++){
-      //this.getDBServer(1);
-      this.getDatabases();
+    //this.getDBServer(1);
+    this.getDatabases();
     //}
   }
-  
-  changeForm(){
-    if(this.showForm === false){
-      this.showForm = true; 
+
+  changeForm() {
+    if (this.showForm === false) {
+      this.showForm = true;
     }
-    else{
+    else {
       this.showForm = false;
     }
   }
-  
-   open(content){
-     this.modalService.open(content);
+
+  open(content) {
+    this.modalService.open(content);
   }
-  
- /* getDBServer(id){
-    this._apiservice.getDBServer(id)
-    .subscribe((data:any) => {
-      this.deviceData = data
-      console.log(this.getDevice);
-    },error => console.log(error));
-  
-  }*/
-   getDatabases() {
-this._apiservice.getDatabases()
+
+  /* getDBServer(id){
+     this._apiservice.getDBServer(id)
+     .subscribe((data:any) => {
+       this.deviceData = data
+       console.log(this.getDevice);
+     },error => console.log(error));
+   
+   }*/
+  getDatabases() {
+    this._apiservice.getDatabases()
       .subscribe((data: any) => {
-       this.displayDevices = data;
-       console.log(this.displayDevices);
+        this.displayDevices = data;
+        
       }, error => {
-     
-        console.log(error);}
+
+        console.log(error);
+      }
       );
   }
-  
-   editorGroup(): void {
-  this.showForm = false; 
-  this.isLol = true;
+
+  editorGroup(): void {
+    this.showForm = false;
+    this.isLol = true;
   }
-  
-  
-  
-  
-   handleSort() {
+
+
+
+
+  handleSort() {
 
     if (!this.desc) {
       this.displayDevices.sort(this.doAsc);
@@ -102,7 +103,7 @@ this._apiservice.getDatabases()
   }
 
   doAsc(a, b) {
-    
+
 
     if (a.hostName > b.hostName) {
       return -1;
@@ -113,7 +114,7 @@ this._apiservice.getDatabases()
   }
 
   doDsc(a, b) {
-   
+
     if (a.hostName < b.hostName) {
       return -1;
     } else if (a.hostName > b.hostName) {
@@ -121,76 +122,71 @@ this._apiservice.getDatabases()
     }
     return 0;
   }
-  
-  
-   getStartDate(value)
-    {
-      if (value.formatted === "") {
-        this.device.licenseStartDt=null;
-      }
-      else {
-        let d = value.formatted;
-        let latest_date =this.datepipe.transform(d, 'yyyy-MM-dd');
-        this.device.licenseStartDt = moment(latest_date).format();
-      }
-      
+
+
+  getStartDate(value) {
+    if (value.formatted === "") {
+      this.device.licenseStartDt = null;
     }
-  
-     getEndDate(value)
-    {
-      if (value.formatted === "") {
-        this.device.licenseEndDt=null;
-      }
-      else {
-        let d = value.formatted;
-        let latest_date =this.datepipe.transform(d, 'yyyy-MM-dd');
-        this.device.licenseEndDt = moment(latest_date).format();
-      }
-      
+    else {
+      let d = value.formatted;
+      let latest_date = this.datepipe.transform(d, 'yyyy-MM-dd');
+      this.device.licenseStartDt = moment(latest_date).format();
     }
-    
-    
-        getRenDate(value)
-    {
-      if (value.formatted === "") {
-        this.device.licenseRenewDt=null;
-      }
-      else {
-        let d = value.formatted;
-     
-        let latest_date =this.datepipe.transform(d, 'yyyy-MM-dd');
-        this.device.licenseRenewDt = moment(latest_date).format();
-      }
-      
+
+  }
+
+  getEndDate(value) {
+    if (value.formatted === "") {
+      this.device.licenseEndDt = null;
     }
-    
-  
-  
-  
-  
-  
-  
-  
-  submitDevice(){
-    console.log("inside submit");
+    else {
+      let d = value.formatted;
+      let latest_date = this.datepipe.transform(d, 'yyyy-MM-dd');
+      this.device.licenseEndDt = moment(latest_date).format();
+    }
+
+  }
+
+
+  getRenDate(value) {
+    if (value.formatted === "") {
+      this.device.licenseRenewDt = null;
+    }
+    else {
+      let d = value.formatted;
+
+      let latest_date = this.datepipe.transform(d, 'yyyy-MM-dd');
+      this.device.licenseRenewDt = moment(latest_date).format();
+    }
+
+  }
+
+
+
+
+
+
+
+
+  submitDevice() {
+
     let url = APP_CONFIG.saveDBServerInfo;
-	this.serverContact1.isPrimary=false
-	this.serverContact.isPrimary=true
+    this.serverContact1.isPrimary = false
+    this.serverContact.isPrimary = true
     this.device.serverContactDTOs.push(this.serverContact);
     this.device.serverContactDTOs.push(this.serverContact1);
-      console.log(JSON.stringify(this.device));
-   this.http.post(url, this.device).subscribe((data: any) => {
-     console.log(JSON.stringify(this.device));
-             
-              console.log(data);
-            }, error => console.log(error));
-    
+
+    this.http.post(url, this.device).subscribe((data: any) => {
+
+    }, error => console.log(error));
+
   }
 
 }
 
- 
- 
+
+
   /*getStartDate(value)
     {
       if (value.formatted === "") {
--- src/app/devicetab/devicetab.component.ts
+++ src/app/devicetab/devicetab.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {APP_CONFIG} from '../app.config';
-import {ApiserviceService} from '../apiservice.service';
-import {Http, HttpModule, Headers, RequestOptions} from '@angular/http';
-import { Device,Server } from '../data_modelDeviceInventory';
+import { APP_CONFIG } from '../app.config';
+import { ApiserviceService } from '../apiservice.service';
+import { Http, HttpModule, Headers, RequestOptions } from '@angular/http';
+import { Device, Server } from '../data_modelDeviceInventory';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UtilService } from '../util.service';
-import { FilterPipe} from '../convertDate.pipe';
+import { FilterPipe } from '../convertDate.pipe';
 import { DatePipe } from '@angular/common';
 import { IMyDate, IMyDpOptions } from 'mydatepicker';
 import * as moment from 'moment';
@@ -17,38 +17,39 @@ import * as moment from 'moment';
 })
 export class DevicetabComponent implements OnInit {
 
-  displayDevices:any;
-    device: Device;
-      public p: number = 1;
+  displayDevices: any;
+  device: Device;
+  public p: number = 1;
   public loading: boolean = false;
   public desc = false;
- constructor(private _apiservice: ApiserviceService, private  http: Http, private modalService: NgbModal, private utilservice: UtilService,private datepipe: DatePipe) {
-  this.device = new Device();
+  constructor(private _apiservice: ApiserviceService, private http: Http, private modalService: NgbModal, private utilservice: UtilService, private datepipe: DatePipe) {
+    this.device = new Device();
 
   }
 
   ngOnInit() {
-      this.getDatabases();
+    this.getDatabases();
   }
-  
- 
-  
-    getDatabases() {
-       this.loading = true;
-this._apiservice.getDatabases()
+
+
+
+  getDatabases() {
+    this.loading = true;
+    this._apiservice.getDatabases()
       .subscribe((data: any) => {
-           this.loading = false;
-       this.displayDevices = data;
-       console.log(this.displayDevices);
+        this.loading = false;
+        this.displayDevices = data;
+
       }, error => {
         this.loading = false;
-        console.log(error);}
+        console.log(error);
+      }
       );
   }
-  
-  
-  
-   handleSort() {
+
+
+
+  handleSort() {
 
     if (!this.desc) {
       this.displayDevices.sort(this.doAsc);
@@ -62,7 +63,7 @@ this._apiservice.getDatabases()
   }
 
   doAsc(a, b) {
-    
+
 
     if (a.hostName > b.hostName) {
       return -1;
@@ -73,7 +74,7 @@ this._apiservice.getDatabases()
   }
 
   doDsc(a, b) {
-   
+
     if (a.hostName < b.hostName) {
       return -1;
     } else if (a.hostName > b.hostName) {
@@ -81,8 +82,8 @@ this._apiservice.getDatabases()
     }
     return 0;
   }
-  
-  
-  
+
+
+
 
 }
