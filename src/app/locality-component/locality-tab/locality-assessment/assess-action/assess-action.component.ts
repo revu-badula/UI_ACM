64d899852ec1f30c1650a09e30934f155394e111
Sushrut Khirwadkar <sushrut.khirwadkar@soch-inc.com>
2018-10-09 17:00:15 -0400

    action plan

--- src/app/locality-component/locality-tab/locality-assessment/assess-action/assess-action.component.ts
+++ src/app/locality-component/locality-tab/locality-assessment/assess-action/assess-action.component.ts
@@ -7,6 +7,7 @@ import { APP_CONFIG } from '../../../../app.config';
 import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 import { AppAssess, AssessmentPolicyDTO, Policy } from '../../../../data.model.assessmentDTO';
 import * as moment from 'moment';
+import { IMyDate, IMyDpOptions } from 'mydatepicker';
 import { DatePipe } from '@angular/common';
 import { FormsModule, NgForm, FormGroup } from '@angular/forms';
 import { Observable, Subject } from 'rxjs';
@@ -23,19 +24,24 @@ export class AssessActionComponent implements OnInit {
   @ViewChild('myForm') myForm: FormGroup;
   appAssess: AppAssess;
   public loading: boolean = false;
-  public appAssessmentDTOs:any;
-  public info:any;
-  public showSave:boolean=false;
-  public editData:any;
-  public showEdit:boolean=false;
-  public showForm:boolean=true;
-  public endDate:any;
-  public startDate:any;
+  public appAssessmentDTOs: any;
+  public info: any;
+  public showSave: boolean = false;
+  public editData: any;
+  public showEdit: boolean = false;
+  public showForm: boolean = true;
+  public endDate: any;
+  public startDate: any;
+  myDatePickerOptions: IMyDpOptions = {
+    disableUntil: { year: 0, month: 0, day: 0 },
+    showTodayBtn: false
+
+  };
   constructor(private _apiservice: ApiserviceService, private utilService: UtilService,
     private http: Http, private router: Router, private modalService: NgbModal, private datepipe: DatePipe) {
-      this.appAssess = new AppAssess();
+    this.appAssess = new AppAssess();
     this.getAppId();
-     }
+  }
 
   ngOnInit() {
   }
@@ -48,169 +54,180 @@ export class AssessActionComponent implements OnInit {
         this.appAssess.applicationID = data.applicationViewDTO.applicationId;
         this.showOnPageLoad();
       }, error => {
-        this.loading=false;
-        console.log(error);});
-    }
+        this.loading = false;
+        console.log(error);
+      });
+  }
 
 
-  showOnPageLoad()
-  {
-    if(localStorage.getItem('assesId') === null)
-    {
-    
+  showOnPageLoad() {
+    if (localStorage.getItem('assesId') === null) {
+
     }
-    else{
-      let id =localStorage.getItem('assesId');
+    else {
+      let id = localStorage.getItem('assesId');
       let auid = +id;
-      this.loading=true;
-      this.showEdit=true;
+      this.loading = true;
+      this.showEdit = true;
       this._apiservice.getAssessData(auid)
-      .subscribe((data: any) => {
-        this.loading=false;
-        this.appAssess=data
+        .subscribe((data: any) => {
+          this.loading = false;
+          this.appAssess = data
+          let day,month,year;
+          if (this.appAssess.actionPlanStartDt === null) {
+            this.startDate = { date: null };
+            this.myForm.controls['endDate'].disable();
+          }
+          else {
+            let d = new Date(this.appAssess.actionPlanStartDt);
+             day = d.getDate();
+             month = d.getMonth() + 1;
+             year = d.getFullYear();
+            this.startDate = { date: { year: year, month: month, day: day } };
+          }
 
-    if(this.appAssess.actionPlanStartDt === null)
-    {
-      this.startDate = {date:null};
+          if (this.appAssess.actionPlanEndDt === null) {
+            this.endDate = { date: null };
+          } else {
+            let dt = new Date(this.appAssess.actionPlanEndDt);
+            let day1 = dt.getDate();
+            let month1 = dt.getMonth() + 1;
+            let year1 = dt.getFullYear();
+            this.endDate = { date: { year: year1, month: month1, day: day1 } };
+            this.myDatePickerOptions.disableUntil.day = day;
+            this.myDatePickerOptions.disableUntil.month = month;
+            this.myDatePickerOptions.disableUntil.year = year;
+            this.myDatePickerOptions.showTodayBtn = false;
+          }
+        }, error => {
+          this.loading = false;
+          console.log(error);
+        });
+
+
+
     }
-    else{
-    let d = new Date(this.appAssess.actionPlanStartDt);
-    let day = d.getDate();
-    let month = d.getMonth()+1;
-    let year = d.getFullYear();
-    this.startDate = {date:{year: year, month: month, day: day}};
-    }
-
-    if(this.appAssess.actionPlanEndDt === null)
-    {
-      this.endDate = {date:null};
-    }else{
-    let dt = new Date(this.appAssess.actionPlanEndDt);
-    let day1 = dt.getDate();
-    let month1 = dt.getMonth()+1;
-    let year1 = dt.getFullYear();
-    this.endDate = {date:{year: year1, month: month1, day: day1}};
-    }},error=> {
-      this.loading=false;
-      console.log(error);});
-
-    
 
   }
-  
-}
 
-saveActionPlan()
-{
-  let ngbModalOptions: NgbModalOptions = {
-    backdrop : 'static',
-    keyboard : false
+  saveActionPlan() {
+    let ngbModalOptions: NgbModalOptions = {
+      backdrop: 'static',
+      keyboard: false
     };
-  this.loading = true;
-  const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
-  let options = new RequestOptions({ headers: headers });
-  let url_update = APP_CONFIG.updateAppAssessment;
-  this.appAssess.updatedBy=Cookie.get('userName');
-  let data = JSON.stringify(this.appAssess);
-  this.http.post(url_update,data,options)
+    this.loading = true;
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let options = new RequestOptions({ headers: headers });
+    let url_update = APP_CONFIG.updateAppAssessment;
+    this.appAssess.updatedBy = Cookie.get('userName');
+    let data = JSON.stringify(this.appAssess);
+    this.http.post(url_update, data, options)
       .subscribe((data: any) => {
-      this.loading=false;
-      const { myForm: { value: formValueSnap } } = this;
+        this.loading = false;
+        const { myForm: { value: formValueSnap } } = this;
         this.myForm.reset(formValueSnap);
-      this.info="Action Plan has been updated.";
-      this.modalService.open(this.content,ngbModalOptions);
+        this.info = "Action Plan has been updated.";
+        this.modalService.open(this.content, ngbModalOptions);
       }, error => {
         this.loading = false;
         console.log(error);
       });
-}
+  }
 
 
-  getStartDate(value)
-    {
-      if (value.formatted === "") {
-        this.appAssess.actionPlanStartDt=null;
-      }
-      else {
-        let d = value.formatted;
-        //this.audate = Date.parse(d);
-        let latest_date =this.datepipe.transform(d, 'yyyy-MM-dd');
-        this.appAssess.actionPlanStartDt = moment(latest_date).format();
-      }
-      
+  getStartDate(value) {
+    this.myForm.controls['endDate'].disable();
+    this.endDate = null;
+    if (value.formatted === "") {
+      this.appAssess.actionPlanStartDt = null;
+    }
+    else {
+     
+      //this.audate = Date.parse(d);
+      let latest_date = this.datepipe.transform(value.formatted, 'yyyy-MM-dd');
+      this.appAssess.actionPlanStartDt = moment(latest_date).format();
+      let d = new Date(value.formatted);
+      let year = d.getFullYear();
+      let month = d.getMonth() + 1;
+      let day = d.getDate();
+      this.myDatePickerOptions.disableUntil.day = day;
+      this.myDatePickerOptions.disableUntil.month = month;
+      this.myDatePickerOptions.disableUntil.year = year;
+      this.myDatePickerOptions.showTodayBtn = false;
+      this.myForm.controls['endDate'].enable();
     }
 
-     
-  getEndDate(value)
-  {
+  }
+
+
+  getEndDate(value) {
     if (value.formatted === "") {
-      this.appAssess.actionPlanEndDt=null;
+      this.appAssess.actionPlanEndDt = null;
     }
     else {
       let d = value.formatted;
       //this.audate = Date.parse(d);
-      let latest_date =this.datepipe.transform(d, 'yyyy-MM-dd');
+      let latest_date = this.datepipe.transform(d, 'yyyy-MM-dd');
       this.appAssess.actionPlanEndDt = moment(latest_date).format();
     }
-    
+
   }
-  valueChanged()
-  {
-    this.showForm=false;
-    this.showSave=true;
-    this.showEdit=false;
+  valueChanged() {
+    this.showForm = false;
+    this.showSave = true;
+    this.showEdit = false;
   }
 
-  showLeft(){
+  showLeft() {
     this.router.navigate(['locality/tab/assessment']);
+  }
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    // console.log(this.myForm);
+    // console.log(this.myForm.dirty);
+    //if (this.myForm.classList[3] === 'ng-touched' || this.myForm.nativeElement.classList[3] === 'ng-dirty') {
+    if (this.myForm.dirty && this.myForm.valid) {
+      //return this.dialogService.confirm('Discard changes for Budget?');
+      //const modal=this.modalService.open(this.content1, ngbModalOptions);
+
+      return this.confirm1('Do you want to save changes?', 'for action plan', 'YES', 'NO');
+
+
     }
-    canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
-      // console.log(this.myForm);
-      // console.log(this.myForm.dirty);
-      //if (this.myForm.classList[3] === 'ng-touched' || this.myForm.nativeElement.classList[3] === 'ng-dirty') {
-      if (this.myForm.dirty) {
-        //return this.dialogService.confirm('Discard changes for Budget?');
-        //const modal=this.modalService.open(this.content1, ngbModalOptions);
-  
-        return this.confirm1('Do you want to save changes?', 'for action plan', 'YES', 'NO');
-  
-  
-      }
-  
-      return true;
-  
-    }
-  
-  
-  
-  
-    confirm1(title = 'Are you sure?', text, confirmButtonText, cancelButtonText, showCancelButton = true) {
-      return new Promise<boolean>((resolve, reject) => {
-        swal({
-          title: title,
-          text: text,
-          type: 'warning',
-          showCancelButton: showCancelButton,
-          confirmButtonText: confirmButtonText,
-          cancelButtonText: cancelButtonText,
-          allowOutsideClick: false
-        }).then((result) => {
-          if (result.value !== undefined && result.value) {
-            this.saveActionPlan();
-            resolve(false);
-          }
-          else {
-            resolve(true);
-          }
-        }, error => reject(error));
-      });
-  
-  
-  
-  
-    }
-  
+
+    return true;
+
+  }
+
+
+
+
+  confirm1(title = 'Are you sure?', text, confirmButtonText, cancelButtonText, showCancelButton = true) {
+    return new Promise<boolean>((resolve, reject) => {
+      swal({
+        title: title,
+        text: text,
+        type: 'warning',
+        showCancelButton: showCancelButton,
+        confirmButtonText: confirmButtonText,
+        cancelButtonText: cancelButtonText,
+        allowOutsideClick: false
+      }).then((result) => {
+        if (result.value !== undefined && result.value) {
+          this.saveActionPlan();
+          resolve(false);
+        }
+        else {
+          resolve(true);
+        }
+      }, error => reject(error));
+    });
+
+
+
+
+  }
+
 
 
 }
