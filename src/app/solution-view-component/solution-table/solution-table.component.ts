82713eba2697de41945e65cd2281520a2562bf28
revu-badula <39134129+revu-badula@users.noreply.github.com>
2018-10-15 15:38:46 -0400

    Solution Sort

--- src/app/solution-view-component/solution-table/solution-table.component.ts
+++ src/app/solution-view-component/solution-table/solution-table.component.ts
@@ -10,6 +10,9 @@ import { FilterPipe } from '../../convertDate.pipe';
 export class SolutionTableComponent implements OnInit {
   public solutions: any;
   public desc: boolean = false;
+  public des: boolean = false;
+  public dec: boolean = false;
+  public de: boolean = false;
   public loading: boolean = false;
   public p: number = 1;
 
@@ -64,5 +67,142 @@ export class SolutionTableComponent implements OnInit {
     }
     return 0;
   }
+
+
+handleSorta() {
+
+    if (!this.des) {
+      this.solutions.sort(this.doAs);
+      this.des = true;
+    }
+    else {
+      this.solutions.sort(this.doDs);
+      this.des = false;
+    }
+
+  }
+
+  doAs(a, b) {
+
+    if (a.vendor.name > b.vendor.name) {
+      return -1;
+    } else if (a.vendor.name < b.vendor.name) {
+      return 1;
+    }
+    return 0;
+  }
+
+  doDs(a, b) {
+    if (a.vendor.name < b.vendor.name) {
+      return -1;
+    } else if (a.vendor.name > b.vendor.name) {
+      return 1;
+    }
+    return 0;
+  }
+  
+  
+  
+  
+  handleSortb() {
+
+    if (!this.dec) {
+      this.solutions.sort(this.doA);
+      this.dec = true;
+    }
+    else {
+      this.solutions.sort(this.doD);
+      this.dec = false;
+    }
+
+  }
+
+  doA(a, b) {
+
+    if (a.solutionTypeName > b.solutionTypeName) {
+      return -1;
+    } else if (a.solutionTypeName < b.solutionTypeName) {
+      return 1;
+    }
+    return 0;
+  }
+
+  doD(a, b) {
+    if (a.solutionTypeName < b.solutionTypeName) {
+      return -1;
+    } else if (a.solutionTypeName > b.solutionTypeName) {
+      return 1;
+    }
+    return 0;
+  }
+  
+  
+  
+  handleSortc() {
+
+    if (!this.de) {
+      this.solutions.sort(this.doAc);
+      this.de = true;
+    }
+    else {
+      this.solutions.sort(this.doDc);
+      this.de = false;
+    }
+
+  }
+
+  doAc(a, b) {
+
+    if (a.systemTypeDTO.name > b.systemTypeDTO.name) {
+      return -1;
+    } else if (a.systemTypeDTO.name < b.systemTypeDTO.name) {
+      return 1;
+    }
+    return 0;
+  }
+
+  doDc(a, b) {
+    if (a.systemTypeDTO.name < b.systemTypeDTO.name) {
+      return -1;
+    } else if (a.systemTypeDTO.name > b.systemTypeDTO.name) {
+      return 1;
+    }
+    return 0;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
