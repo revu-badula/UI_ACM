6bd8a050f4bccfc6859d081af8d357bf476c682d
unknown <revathi.badula@soch-inc.com>
2019-01-21 12:05:08 -0500

    config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 
-//const baseUrl = 'http://localhost:8080/ApplicationPortfolioManager/services/';
+const baseUrl = 'http://localhost:8080/ApplicationPortfolioManager/services/';
 
 //const baseUrl = 'http://ec2-54-86-177-206.compute-1.amazonaws.com/ApplicationPortfolioManager/services/';
 
 
-const baseUrl='http://172.24.16.56:8080/ApplicationPortfolioManager/services/';
+//const baseUrl='http://172.24.16.56:8080/ApplicationPortfolioManager/services/';
 
 //const baseUrl = 'http://23.96.86.203:8080/ApplicationPortfolioManager/services/';
 
