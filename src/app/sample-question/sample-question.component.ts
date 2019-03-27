57f2905b14428eada9a3bf3876987f103adc317b
unknown <revathi.badula@soch-inc.com>
2019-03-27 11:22:58 -0400

    sample

--- src/app/sample-question/sample-question.component.ts
+++ src/app/sample-question/sample-question.component.ts
@@ -4,56 +4,15 @@ import { IMyDpOptions } from 'mydatepicker';
 
 import { NgModule, enableProdMode } from '@angular/core'
 
-import { BrowserModule } from '@angular/platform-browser';
-import { FusionChartsModule } from 'angular-fusioncharts';
 
-// Load FusionCharts
-import * as FusionCharts from 'fusioncharts';
-// Load Charts module
-import * as Charts from 'fusioncharts/fusioncharts.charts';
-// Load fusion theme
-import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
 import { NgbModal}from '@ng-bootstrap/ng-bootstrap';
 
 import { UtilService } from '../util.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-FusionChartsModule.fcRoot(FusionCharts, Charts)
 
 
-const data = {
-  "chart": {
-    "caption": "Evidence",
-    "subcaption": "",
-    "showvalues": "1",
-    "showpercentintooltip": "0",
-    "numberprefix": "",
-    "enablemultislicing": "1",
-    "theme": "fusion"
-  },
-  "data": [
-    {
-      "label": "Pass",
-      "value": "30"
-    },
-    {
-      "label": "Fail",
-      "value": "23"
-    },
-    // {
-    //   "label": "Bullion",
-    //   "value": "180000"
-    // },
-    // {
-    //   "label": "Real-estate",
-    //   "value": "270000"
-    // },
-    // {
-    //   "label": "Insurance",
-    //   "value": "20000"
-    // }
-  ]
-};
+
 
 @Component({
   selector: 'app-sample-question',
@@ -66,20 +25,14 @@ const data = {
  
   ],
   imports: [
-    BrowserModule,
-    FusionChartsModule // Include in imports
+   
   ],
 
 })
 
 
 export class SampleQuestionComponent implements OnInit {
-  data: Object;
-  width = 400;
-  height = 300;
-  type = 'pie3d';
-  dataFormat = 'json';
-  dataSource = data;
+
 
 
 
